77a091da3b22eeafef9aacbab91485a2ef338ad0
agarmash <forwebsmmm@gmail.com>
2020-05-24 23:55:10 +0300

    library customer

--- library/models/models.py
+++ library/models/models.py
@@ -15,6 +15,8 @@ class LibBook(models.Model):
     author_ids = fields.Many2many('lib.author', string='Authors')
     editor_ids = fields.Many2many('lib.editor', string='Editors')
     isbn = fields.Char('ISBN')
+    history = fields.One2many('lib.history', 'book_id', string='Book history')
+    customer_id = fields.Many2one('lib.customer', string='Customer')
 
 
 class LibAuthor(models.Model):
@@ -37,11 +39,13 @@ class LibCustomer(models.Model):
     surname = fields.Char('Surname')
     address = fields.Char('Address')
     email = fields.Char('Email')
+    book_ids = fields.One2many('lib.book', 'customer_id', string='Books')
 
 class LibHistory(models.Model):
     _name = 'lib.history'
 
     book_id = fields.Many2one('lib.book', string='Book')
-    date = fields.Datetime(string='Date')
-    inlib = fields.Boolean(string='Book in Library?', default=True)
+    date_out = fields.Datetime(string='Date out', default=fields.Datetime.now)
+    date_in = fields.Datetime(string='Date in')
+    # inlib = fields.Boolean(string='Book in Library?', default=True)
     customer_id = fields.Many2one('lib.customer', string='Customer')
