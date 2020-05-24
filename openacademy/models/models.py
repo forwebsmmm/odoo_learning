4e5ef3b52114b714f6c98dbcada272e5309796c9
agarmash <forwebsmmm@gmail.com>
2020-05-24 15:01:07 +0300

    openacademy

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -15,8 +15,7 @@ class AcademyClass(models.Model):
     ], string='Level')
     session_ids = fields.One2many('academy.session',
                                   'class_id',
-                                  string='Sessions',
-                                  ondelete='cascade')
+                                  string='Sessions')
 
 
 class AcademySession(models.Model):
@@ -24,9 +23,26 @@ class AcademySession(models.Model):
 
     name = fields.Char('Name')
     subject = fields.Char('Subject')
-    # instructor_id = fields.Many2one('res.partner', string='Maesters')
+    instructor_id = fields.Many2one('academy.instructor', string='Maesters')
     start_date = fields.Datetime(string='Start date')
     room_size = fields.Integer(string='Room size')
-    # student_ids = fields.Many2many('res.partner', string='Attendees')
-    class_id = fields.Many2one('academy.class', string='Class')
-    # active = fields.Boolean(string='Active?', default=True)
+    student_ids = fields.Many2many('academy.student', string='Attendees')
+    class_id = fields.Many2one('academy.class', string='Class',
+                               ondelete='cascade')
+    active = fields.Boolean(string='Active?', default=True)
+    state = fields.Selection(
+        [('draft', 'Preparation'),
+         ('open', 'Prepared')],
+        'State', default='draft')
+
+class AcademyInstructor(models.Model):
+    _name = 'academy.instructor'
+
+    name = fields.Char('Name')
+    surname = fields.Char('Surname')
+
+class AcademyStudent(models.Model):
+    _name = 'academy.student'
+
+    name = fields.Char('Name')
+    surname = fields.Char('Surname')
